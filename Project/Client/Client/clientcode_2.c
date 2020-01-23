db7d014d384538987f9e18527315bbabb13a2a37
Hossein <hossein.dehghanipour1998@gmail.com>
2020-01-23 19:56:15 +0330

    Added Client CodeVision Code

---
+++ Project/Client/Client/clientcode_2.c
@@ -0,0 +1,404 @@
+/*******************************************************
+This program was created by the
+CodeWizardAVR V3.12 Advanced
+Automatic Program Generator
+� Copyright 1998-2014 Pavel Haiduc, HP InfoTech s.r.l.
+http://www.hpinfotech.com
+
+Project :
+Version :
+Date    : 1/23/2020
+Author  :
+Company :
+Comments:
+
+
+Chip type               : ATmega32
+Program type            : Application
+AVR Core Clock frequency: 8.000000 MHz
+Memory model            : Small
+External RAM size       : 0
+Data Stack size         : 512
+*******************************************************/
+
+#include <mega32.h>
+#include <delay.h>
+#include <delay.h>
+#include <stdio.h>
+#include <string.h>
+// Alphanumeric LCD functions
+#include <alcd.h>
+
+// Declare your global variables here
+int temperatureAmount = 0 ;
+char dataString[41] = "" ;
+char data ;
+char header[8] = "10101010" ; // 170
+char headerAmount = 170 ;
+char footerAmount = 225 ;
+char footer[8] = "11111111" ; //255
+int packetNo = 0 ;
+char inputData = 0 ;
+int motorPWM = 0 ;
+
+#define DATA_REGISTER_EMPTY (1<<UDRE)
+#define RX_COMPLETE (1<<RXC)
+#define FRAMING_ERROR (1<<FE)
+#define PARITY_ERROR (1<<UPE)
+#define DATA_OVERRUN (1<<DOR)
+
+// USART Receiver buffer
+#define RX_BUFFER_SIZE 8
+char rx_buffer[RX_BUFFER_SIZE];
+
+#if RX_BUFFER_SIZE <= 256
+unsigned char rx_wr_index=0,rx_rd_index=0;
+#else
+unsigned int rx_wr_index=0,rx_rd_index=0;
+#endif
+
+#if RX_BUFFER_SIZE < 256
+unsigned char rx_counter=0;
+#else
+unsigned int rx_counter=0;
+#endif
+
+// This flag is set on USART Receiver buffer overflow
+bit rx_buffer_overflow;
+
+// USART Receiver interrupt service routine
+interrupt [USART_RXC] void usart_rx_isr(void)
+{
+char status,data;
+status=UCSRA;
+data=UDR;
+if ((status & (FRAMING_ERROR | PARITY_ERROR | DATA_OVERRUN))==0)
+   {
+   rx_buffer[rx_wr_index++]=data;
+#if RX_BUFFER_SIZE == 256
+   // special case for receiver buffer size=256
+   if (++rx_counter == 0) rx_buffer_overflow=1;
+#else
+   if (rx_wr_index == RX_BUFFER_SIZE) rx_wr_index=0;
+   if (++rx_counter == RX_BUFFER_SIZE)
+      {
+      rx_counter=0;
+      rx_buffer_overflow=1;
+      }
+#endif
+   }
+}
+
+#ifndef _DEBUG_TERMINAL_IO_
+// Get a character from the USART Receiver buffer
+#define _ALTERNATE_GETCHAR_
+#pragma used+
+char getchar(void)
+{
+char data;
+while (rx_counter==0);
+data=rx_buffer[rx_rd_index++];
+#if RX_BUFFER_SIZE != 256
+if (rx_rd_index == RX_BUFFER_SIZE) rx_rd_index=0;
+#endif
+#asm("cli")
+--rx_counter;
+#asm("sei")
+return data;
+}
+#pragma used-
+#endif
+
+// USART Transmitter buffer
+#define TX_BUFFER_SIZE 8
+char tx_buffer[TX_BUFFER_SIZE];
+
+#if TX_BUFFER_SIZE <= 256
+unsigned char tx_wr_index=0,tx_rd_index=0;
+#else
+unsigned int tx_wr_index=0,tx_rd_index=0;
+#endif
+
+#if TX_BUFFER_SIZE < 256
+unsigned char tx_counter=0;
+#else
+unsigned int tx_counter=0;
+#endif
+
+// USART Transmitter interrupt service routine
+interrupt [USART_TXC] void usart_tx_isr(void)
+{
+if (tx_counter)
+   {
+   --tx_counter;
+   UDR=tx_buffer[tx_rd_index++];
+#if TX_BUFFER_SIZE != 256
+   if (tx_rd_index == TX_BUFFER_SIZE) tx_rd_index=0;
+#endif
+   }
+}
+
+#ifndef _DEBUG_TERMINAL_IO_
+// Write a character to the USART Transmitter buffer
+#define _ALTERNATE_PUTCHAR_
+#pragma used+
+void putchar(char c)
+{
+while (tx_counter == TX_BUFFER_SIZE);
+#asm("cli")
+if (tx_counter || ((UCSRA & DATA_REGISTER_EMPTY)==0))
+   {
+   tx_buffer[tx_wr_index++]=c;
+#if TX_BUFFER_SIZE != 256
+   if (tx_wr_index == TX_BUFFER_SIZE) tx_wr_index=0;
+#endif
+   ++tx_counter;
+   }
+else
+   UDR=c;
+#asm("sei")
+}
+#pragma used-
+#endif
+
+// Standard Input/Output functions
+#include <stdio.h>
+
+// Timer 0 overflow interrupt service routine
+interrupt [TIM0_OVF] void timer0_ovf_isr(void)
+{
+// Place your code here
+
+}
+
+// Timer 0 output compare interrupt service routine
+interrupt [TIM0_COMP] void timer0_comp_isr(void)
+{
+// Place your code here
+
+}
+
+#define FIRST_ADC_INPUT 0
+#define LAST_ADC_INPUT 2
+unsigned char adc_data[LAST_ADC_INPUT-FIRST_ADC_INPUT+1];
+// Voltage Reference: AREF pin
+#define ADC_VREF_TYPE ((0<<REFS1) | (0<<REFS0) | (1<<ADLAR))
+
+// ADC interrupt service routine
+// with auto input scanning
+interrupt [ADC_INT] void adc_isr(void)
+{
+static unsigned char input_index=0;
+// Read the 8 most significant bits
+// of the AD conversion result
+adc_data[input_index]=ADCH;
+// Select next ADC input
+if (++input_index > (LAST_ADC_INPUT-FIRST_ADC_INPUT))
+   input_index=0;
+ADMUX=(FIRST_ADC_INPUT | ADC_VREF_TYPE)+input_index;
+// Delay needed for the stabilization of the ADC input voltage
+delay_us(10);
+// Start the AD conversion
+ADCSRA|=(1<<ADSC);
+}
+
+int getTemp(){
+  int a = adc_data[0] ;
+  int temp  = a/2.054 ;
+  return temp ;
+}
+
+
+
+void main(void)
+{
+// Declare your local variables here
+
+// Input/Output Ports initialization
+// Port A initialization
+// Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=In Bit2=In Bit1=In Bit0=In
+DDRA=(0<<DDA7) | (0<<DDA6) | (0<<DDA5) | (0<<DDA4) | (0<<DDA3) | (0<<DDA2) | (0<<DDA1) | (0<<DDA0);
+// State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=T Bit0=T
+PORTA=(0<<PORTA7) | (0<<PORTA6) | (0<<PORTA5) | (0<<PORTA4) | (0<<PORTA3) | (0<<PORTA2) | (0<<PORTA1) | (0<<PORTA0);
+
+// Port B initialization
+// Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=Out Bit2=In Bit1=In Bit0=In
+DDRB=(0<<DDB7) | (0<<DDB6) | (0<<DDB5) | (0<<DDB4) | (1<<DDB3) | (0<<DDB2) | (0<<DDB1) | (0<<DDB0);
+// State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=0 Bit2=T Bit1=T Bit0=T
+PORTB=(0<<PORTB7) | (0<<PORTB6) | (0<<PORTB5) | (0<<PORTB4) | (0<<PORTB3) | (0<<PORTB2) | (0<<PORTB1) | (0<<PORTB0);
+
+// Port C initialization
+// Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=In Bit2=In Bit1=In Bit0=In
+DDRC=(0<<DDC7) | (0<<DDC6) | (0<<DDC5) | (0<<DDC4) | (0<<DDC3) | (0<<DDC2) | (0<<DDC1) | (0<<DDC0);
+// State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=T Bit0=T
+PORTC=(0<<PORTC7) | (0<<PORTC6) | (0<<PORTC5) | (0<<PORTC4) | (0<<PORTC3) | (0<<PORTC2) | (0<<PORTC1) | (0<<PORTC0);
+
+// Port D initialization
+// Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=In Bit2=In Bit1=In Bit0=In
+DDRD=(0<<DDD7) | (0<<DDD6) | (0<<DDD5) | (0<<DDD4) | (0<<DDD3) | (0<<DDD2) | (0<<DDD1) | (0<<DDD0);
+// State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=T Bit0=T
+PORTD=(0<<PORTD7) | (0<<PORTD6) | (0<<PORTD5) | (0<<PORTD4) | (0<<PORTD3) | (0<<PORTD2) | (0<<PORTD1) | (0<<PORTD0);
+
+// Timer/Counter 0 initialization
+// Clock source: System Clock
+// Clock value: 125.000 kHz
+// Mode: Phase correct PWM top=0xFF
+// OC0 output: Non-Inverted PWM
+// Timer Period: 4.08 ms
+// Output Pulse(s):
+// OC0 Period: 4.08 ms Width: 1.536 ms
+TCCR0=(1<<WGM00) | (1<<COM01) | (0<<COM00) | (0<<WGM01) | (0<<CS02) | (1<<CS01) | (1<<CS00);
+TCNT0=0x00;
+OCR0=0x60;
+
+// Timer/Counter 1 initialization
+// Clock source: System Clock
+// Clock value: Timer1 Stopped
+// Mode: Normal top=0xFFFF
+// OC1A output: Disconnected
+// OC1B output: Disconnected
+// Noise Canceler: Off
+// Input Capture on Falling Edge
+// Timer1 Overflow Interrupt: Off
+// Input Capture Interrupt: Off
+// Compare A Match Interrupt: Off
+// Compare B Match Interrupt: Off
+TCCR1A=(0<<COM1A1) | (0<<COM1A0) | (0<<COM1B1) | (0<<COM1B0) | (0<<WGM11) | (0<<WGM10);
+TCCR1B=(0<<ICNC1) | (0<<ICES1) | (0<<WGM13) | (0<<WGM12) | (0<<CS12) | (0<<CS11) | (0<<CS10);
+TCNT1H=0x00;
+TCNT1L=0x00;
+ICR1H=0x00;
+ICR1L=0x00;
+OCR1AH=0x00;
+OCR1AL=0x00;
+OCR1BH=0x00;
+OCR1BL=0x00;
+
+// Timer/Counter 2 initialization
+// Clock source: System Clock
+// Clock value: Timer2 Stopped
+// Mode: Normal top=0xFF
+// OC2 output: Disconnected
+ASSR=0<<AS2;
+TCCR2=(0<<PWM2) | (0<<COM21) | (0<<COM20) | (0<<CTC2) | (0<<CS22) | (0<<CS21) | (0<<CS20);
+TCNT2=0x00;
+OCR2=0x00;
+
+// Timer(s)/Counter(s) Interrupt(s) initialization
+TIMSK=(0<<OCIE2) | (0<<TOIE2) | (0<<TICIE1) | (0<<OCIE1A) | (0<<OCIE1B) | (0<<TOIE1) | (1<<OCIE0) | (1<<TOIE0);
+
+// External Interrupt(s) initialization
+// INT0: Off
+// INT1: Off
+// INT2: Off
+MCUCR=(0<<ISC11) | (0<<ISC10) | (0<<ISC01) | (0<<ISC00);
+MCUCSR=(0<<ISC2);
+
+// USART initialization
+// Communication Parameters: 8 Data, 2 Stop, No Parity
+// USART Receiver: On
+// USART Transmitter: On
+// USART Mode: Asynchronous
+// USART Baud Rate: 9600
+UCSRA=(0<<RXC) | (0<<TXC) | (0<<UDRE) | (0<<FE) | (0<<DOR) | (0<<UPE) | (0<<U2X) | (0<<MPCM);
+UCSRB=(1<<RXCIE) | (1<<TXCIE) | (0<<UDRIE) | (1<<RXEN) | (1<<TXEN) | (0<<UCSZ2) | (0<<RXB8) | (0<<TXB8);
+UCSRC=(1<<URSEL) | (0<<UMSEL) | (0<<UPM1) | (0<<UPM0) | (1<<USBS) | (1<<UCSZ1) | (1<<UCSZ0) | (0<<UCPOL);
+UBRRH=0x00;
+UBRRL=0x33;
+
+// Analog Comparator initialization
+// Analog Comparator: Off
+// The Analog Comparator's positive input is
+// connected to the AIN0 pin
+// The Analog Comparator's negative input is
+// connected to the AIN1 pin
+ACSR=(1<<ACD) | (0<<ACBG) | (0<<ACO) | (0<<ACI) | (0<<ACIE) | (0<<ACIC) | (0<<ACIS1) | (0<<ACIS0);
+
+// ADC initialization
+// ADC Clock frequency: 1000.000 kHz
+// ADC Voltage Reference: AREF pin
+// ADC Auto Trigger Source: ADC Stopped
+// Only the 8 most significant bits of
+// the AD conversion result are used
+ADMUX=FIRST_ADC_INPUT | ADC_VREF_TYPE;
+ADCSRA=(1<<ADEN) | (1<<ADSC) | (0<<ADATE) | (0<<ADIF) | (1<<ADIE) | (0<<ADPS2) | (1<<ADPS1) | (1<<ADPS0);
+SFIOR=(0<<ADTS2) | (0<<ADTS1) | (0<<ADTS0);
+
+// SPI initialization
+// SPI disabled
+SPCR=(0<<SPIE) | (0<<SPE) | (0<<DORD) | (0<<MSTR) | (0<<CPOL) | (0<<CPHA) | (0<<SPR1) | (0<<SPR0);
+
+// TWI initialization
+// TWI disabled
+TWCR=(0<<TWEA) | (0<<TWSTA) | (0<<TWSTO) | (0<<TWEN) | (0<<TWIE);
+
+// Alphanumeric LCD initialization
+// Connections are specified in the
+// Project|Configure|C Compiler|Libraries|Alphanumeric LCD menu:
+// RS - PORTC Bit 0
+// RD - PORTC Bit 1
+// EN - PORTC Bit 2
+// D4 - PORTC Bit 4
+// D5 - PORTC Bit 5
+// D6 - PORTC Bit 6
+// D7 - PORTC Bit 7
+// Characters/line: 16
+lcd_init(16);
+
+// Global enable interrupts
+#asm("sei")
+
+while (1)
+      {
+        inputData = getchar();
+        if ( int(inputData) == 170 ){
+          //send temperature data
+          temperatureAmount = getTemp();
+          //Process :
+           int tempMode = temperatureAmount % 255 ;
+           int tempDiv = temperatureAmount / 255 ;
+
+           if ( tempMode == 0 && tempDiv != 0 ){
+             packetNo = 1 ;
+             putchar(headerAmount);
+             delay_ms(1);
+             putchar(char(packetNo));
+             delay_ms(1);
+             putchar(tempDiv);
+             delay_ms(1);
+             putchar(footerAmount);
+           }
+           else if ( tempMode != 0 && tempDiv == 0 ){
+             packetNo = 1 ;
+             putchar(headerAmount);
+             delay_ms(1);
+             putchar(char(packetNo));
+             delay_ms(1);
+             putchar(tempMode);
+             delay_ms(1);
+             putchar(footerAmount);
+           }
+           else if ( tempMode != 0 && tempDiv != 0  ){
+             packetNo = 2 ;
+             putchar(headerAmount);
+             delay_ms(1);
+             putchar(char(packetNo));
+             delay_ms(1);
+             putchar(tempDiv);
+             delay_ms(1);
+             putchar(tempMode);
+             delay_ms(1);
+             putchar(footerAmount);
+           }
+        }
+
+        else if ( int(inputData) == 100 ){
+          // set motor pwm
+          getchar();
+          motorPWM = getchar();
+          OCR0 = (255*motorPWM)/100 ;
+          getchar();
+        }
+      // Place your code here
+      }
+}
